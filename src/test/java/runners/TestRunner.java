5428feb9490e158eaf55321dffeebc806ee0fbe6
Rawoof <rawoof@dmvtek>
2026-01-27 20:19:05 -0500

    testing
    testing

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,17 +1,11 @@
 package runners;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import com.intuit.karate.junit5.Karate;
 
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue ="stepdefinitions",
-        plugin = {
-        "pretty",
-        "html:target/cucumber-report.html"
-        }
-
-)
-
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner {
+    
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run("classpath:features").relativeTo(getClass());
+    }
 }
